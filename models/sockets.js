66776fb5ca5c775ac11c87329b996482fecf5ec0
Mary Martinez <maria.martinez580@misena.edu.co>
2022-09-26 16:16:04 -0500

    band-list1

---
+++ models/sockets.js
@@ -0,0 +1,55 @@
+const BandList  = require ('./band-list');
+
+class Sockets {
+
+    constructor( io ) {
+
+        this.io = io;
+
+        this.BandList = new BandList();
+
+        this.socketEvents();
+    }
+
+    socketEvents() {
+        // On connection
+        this.io.on('connection', ( socket ) => {
+
+            console.log('cliente conectado');
+
+            //emitir al cliente conectado a todas las bandas actuales
+            socket.emit ('hola', this.bandList.getBands());
+
+            //votar por la banda
+            socket.on ('votar-banda',(id) =>{
+                this.bandList.increaseVotes(id);
+                this.io.emit('hola',this.bandList.getBands() );
+            });
+
+            //borrar banda
+            socket.on ('borrar-banda',(id) =>{
+                this.bandList.removeBand(id);
+                this.io.emit('hola',this.bandList.getBands() );
+            });
+            
+            //cambiar nombre de banda
+            socket.on ('cambiar-nombre-banda',(id) =>{
+                this.bandList.changeName(id,nombre);
+                this.io.emit('hola',this.bandList.getBands() );
+            });
+
+            //crear nueva banda
+            socket.on( 'crear-banda', ({ nombre }) => {
+                this.bandList.addBand( nombre );
+                this.io.emit( 'hola' , this.bandList.getBands() );
+            });
+           
+        
+        });
+    }
+
+
+}
+
+
+module.exports = Sockets;
